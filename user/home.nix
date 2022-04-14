365dfad3e3b48547ef0504663d5b6af9c83ad891
michelav <michel.vasconcelos@gmail.com>
2022-04-14 14:21:23 -0300

    Add Delta Tool

--- user/home.nix
+++ user/home.nix
@@ -45,6 +45,29 @@
       enable = true;
       userName  = "michelav";
       userEmail = "michel.vasconcelos@gmail.com";
+      delta = {
+         enable = true;   
+         options = {
+            navigate = true;
+            # line-numbers = true;
+            syntax-theme = "Dracula";
+            features = "side-by-side line-numbers decorations";
+            plus-style = "syntax #003800";
+            minus-style = "syntax #3f0001";
+            decorations = {
+              commit-decoration-style = "bold yellow box ul";
+              file-style = "bold yellow ul";
+              file-decoration-style = "none";
+              hunk-header-decoration-style = "cyan box ul";
+            };
+            line-numbers = {
+              line-numbers-left-style = "cyan";
+              line-numbers-right-style = "cyan";
+              line-numbers-minus-style = 124;
+              line-numbers-plus-style = 28;
+            };
+         };
+      };
     };
     vscode = {
       enable = true;
