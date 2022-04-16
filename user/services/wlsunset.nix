a80e156efedc284eabae7f8a7413a225576affc5
michelav <michel.vasconcelos@gmail.com>
2022-04-15 23:22:16 -0300

    Add Wlsunset Service

--- user/services/wlsunset.nix
+++ user/services/wlsunset.nix
@@ -1,15 +1,11 @@
-{ pkgs, ... }:
+{ lib, pkgs, config, ... }:
 
 {
-  config = {
-    home-manager.users.michel = { pkgs, ... }: {
-      services.wlsunset = {
-        enable = true;
-        longitude = "-3.75";
-        latitude = "-38.54";
-        temperature.day = 6500;
-        temperature.night = 3500;
-      };
-    };
+  services.wlsunset = {
+    enable = true;
+    longitude = "-3.75";
+    latitude = "-38.54";
+    temperature.day = 6500;
+    temperature.night = 4000;
   };
 }
