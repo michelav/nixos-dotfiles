a5dfea09a6077bfd8990d20e64608489c7de2125
michelav <michel.vasconcelos@gmail.com>
2022-04-24 18:26:31 -0300

    Change Fish Prompt

--- home/fish.nix
+++ home/fish.nix
@@ -6,6 +6,27 @@
     starship = {
       enable = true;
       settings = {
+        username = {
+          format = "[── $user]($style)@";
+          style_user = "bold blue";
+          style_root = "bold red";
+          show_always = true;
+        };
+
+        hostname = {
+          format = "[$hostname]($style) in ";
+          style = "bold dimmed blue";
+          trim_at = "-";
+          ssh_only = false;
+          disabled = false;
+        };
+
+        directory = {
+          truncation_length = 0;
+          truncate_to_repo = true;
+          truncation_symbol = "repo:";
+        };
+
         aws.symbol = "  ";
         buf.symbol = " ";
         conda.symbol = " ";
@@ -14,7 +35,7 @@
         docker_context.symbol = " ";
         elixir.symbol = " ";
         elm.symbol = " ";
-        git_branch.symbol = " ";
+        git_branch.symbol = " ";
         golang.symbol = " ";
         haskell.symbol = " ";
         hg_branch.symbol = " ";
