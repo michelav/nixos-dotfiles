9bea8088d6447413f1572057eb1766aea2527d49
michelav <michel.vasconcelos@gmail.com>
2022-05-03 18:51:16 -0300

    Enable Firmware Updates

--- hosts/vega/default.nix
+++ hosts/vega/default.nix
@@ -169,6 +169,8 @@ in
 	'';
   };
 
+  hardware.enableRedistributableFirmware = true;
+
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
