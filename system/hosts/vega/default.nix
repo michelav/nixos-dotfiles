7d39840d89c91ceefd0a9b91c4d012fc021dee53
michelav <michel.vasconcelos@gmail.com>
2022-06-28 11:18:08 -0300

    Add gaming capabilities

--- system/hosts/vega/default.nix
+++ system/hosts/vega/default.nix
@@ -5,6 +5,7 @@
       ./hardware-configuration.nix
       ../../nixos
       ../../nixos/nvidia.nix
+      ../../nixos/gaming.nix
     ] ++ lib.optional ( null != desktop ) ../../nixos/${desktop}.nix;
 
   networking.networkmanager.enable = true;
