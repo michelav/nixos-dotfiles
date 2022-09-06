4a02dcbabf0faf1746d9584d624d906927e6da47
michelav <michel.vasconcelos@gmail.com>
2022-09-06 00:01:50 -0300

    Change system deprecated property to nixpkgs.hostPlatform

--- system/hosts/vega/hardware-configuration.nix
+++ system/hosts/vega/hardware-configuration.nix
@@ -4,31 +4,27 @@
 { config, lib, pkgs, modulesPath, ... }:
 
 {
-  imports =
-    [ (modulesPath + "/installer/scan/not-detected.nix")
-    ];
+  imports = [ (modulesPath + "/installer/scan/not-detected.nix") ];
 
-  boot.initrd.availableKernelModules = [ "xhci_pci" "ahci" "nvme" "usbhid" "usb_storage" "sd_mod" ];
+  boot.initrd.availableKernelModules =
+    [ "xhci_pci" "ahci" "nvme" "usbhid" "usb_storage" "sd_mod" ];
   boot.initrd.kernelModules = [ ];
   boot.kernelModules = [ "kvm-intel" "acpi_call" ];
-  boot.extraModulePackages = with config.boot.kernelPackages; [ acpi_call ]; 
+  boot.extraModulePackages = with config.boot.kernelPackages; [ acpi_call ];
 
-  fileSystems."/" =
-    { device = "/dev/disk/by-label/VEGA_ROOT";
-      fsType = "ext4";
-    };
+  fileSystems."/" = {
+    device = "/dev/disk/by-label/VEGA_ROOT";
+    fsType = "ext4";
+  };
 
-  fileSystems."/boot" =
-    { device = "/dev/disk/by-label/SYSTEM_DRV";
-      fsType = "vfat";
-    };
+  fileSystems."/boot" = {
+    device = "/dev/disk/by-label/SYSTEM_DRV";
+    fsType = "vfat";
+  };
 
-  swapDevices = [ 
-  {
-    device = "/.swapfile";
-  }
-  ];
-
+  swapDevices = [{ device = "/.swapfile"; }];
+  nixpkgs.hostPlatform.system = "x86_64-linux";
   powerManagement.cpuFreqGovernor = lib.mkDefault "powersave";
-  hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
+  hardware.cpu.intel.updateMicrocode =
+    lib.mkDefault config.hardware.enableRedistributableFirmware;
 }
