039ffab91e1ede894cf02e6fcac9a08b0c0b4f5b
michelav <michel.vasconcelos@gmail.com>
2022-11-25 15:27:55 -0300

    Change git package to full

--- home/michel/cli/git.nix
+++ home/michel/cli/git.nix
@@ -1,38 +1,37 @@
-{
-   programs.git = {
-   enable = true;
-   userName  = "michelav";
-   userEmail = "michel.vasconcelos@gmail.com";
-   aliases = {
-     co = "checkout";
-     graph = "log --decorate --oneline --graph";
-     st = "status";
-   };
-   extraConfig = {
-    init = { defaultBranch = "main"; };
-   };
-   delta = {
+{ pkgs, ... }: {
+  programs.git = {
+    enable = true;
+    package = pkgs.gitAndTools.gitFull;
+    userName = "michelav";
+    userEmail = "michel.vasconcelos@gmail.com";
+    aliases = {
+      co = "checkout";
+      graph = "log --decorate --oneline --graph";
+      st = "status";
+    };
+    extraConfig = { init = { defaultBranch = "main"; }; };
+    delta = {
       enable = true;
       options = {
-         navigate = true;
-         # line-numbers = true;
-         syntax-theme = "Dracula";
-         features = "side-by-side line-numbers decorations";
-         plus-style = "syntax #003800";
-         minus-style = "syntax #3f0001";
-         decorations = {
-           commit-decoration-style = "bold yellow box ul";
-           file-style = "bold yellow ul";
-           file-decoration-style = "none";
-           hunk-header-decoration-style = "cyan box ul";
-         };
-         line-numbers = {
-           line-numbers-left-style = "cyan";
-           line-numbers-right-style = "cyan";
-           line-numbers-minus-style = 124;
-           line-numbers-plus-style = 28;
-         };
+        navigate = true;
+        # line-numbers = true;
+        syntax-theme = "Dracula";
+        features = "side-by-side line-numbers decorations";
+        plus-style = "syntax #003800";
+        minus-style = "syntax #3f0001";
+        decorations = {
+          commit-decoration-style = "bold yellow box ul";
+          file-style = "bold yellow ul";
+          file-decoration-style = "none";
+          hunk-header-decoration-style = "cyan box ul";
+        };
+        line-numbers = {
+          line-numbers-left-style = "cyan";
+          line-numbers-right-style = "cyan";
+          line-numbers-minus-style = 124;
+          line-numbers-plus-style = 28;
+        };
       };
-   };
- };
+    };
+  };
 }
