b4ee0202fc7e96d32098592afec351971e96ceca
michelav <michel.vasconcelos@gmail.com>
2022-11-29 16:20:03 -0300

    Configure user passwd with sops entries

--- system/users/michel.nix
+++ system/users/michel.nix
@@ -1,20 +1,19 @@
 { pkgs, config, ... }: {
-  # Define a user account. Don't forget to set a password with ‘passwd’.
-  # sops.secrets.michel-passwd = {
-  #   sopsFile = ../secrets/vega.yaml;
-  #   neededForUsers = true;
-  # };
+  sops.secrets.michel-passwd = {
+    sopsFile = ../secrets/vega.yaml;
+    neededForUsers = true;
+  };
   # users.mutableUsers = false;
   users.users.michel = {
     isNormalUser = true;
-password = "pass";
-    # passwordFile = config.sops.secrets.michel-passwd.path;
+    # password = "pass";
+    passwordFile = config.sops.secrets.michel-passwd.path;
     shell = pkgs.fish;
     extraGroups = [ "wheel" "video" "audio" ]
       ++ (if config.networking.networkmanager.enable then
-      [ "networkmanager" ]
-    else
-      [ ])
+        [ "networkmanager" ]
+      else
+        [ ])
       ++ (if config.virtualisation.docker.enable then [ "docker" ] else [ ]);
   };
 }
