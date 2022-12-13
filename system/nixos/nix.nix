ed57a7a75026a93f688fe8ab762bcbb24ab6a6f4
michelav <michel.vasconcelos@gmail.com>
2022-12-13 15:44:04 -0300

    Add cache for haskell.nix

--- system/nixos/nix.nix
+++ system/nixos/nix.nix
@@ -6,11 +6,13 @@
         "https://misterio.cachix.org"
         "https://michelav.cachix.org"
         "https://devenv.cachix.org"
+        "https://cache.iog.io"
       ];
       trusted-public-keys = [
         "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
         "michelav.cachix.org-1:nWCV2A3/ZUVYtNcJqzNv4nGxpNipH4aYJ4XQ2ZCQuIM="
         "devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw="
+        "hydra.iohk.io:f/Ea+s+dFdN+3Y/G+FDgSq+a5NEWhJGzdjvKNGv0/EQ="
       ];
       trusted-users = [ "root" "@wheel" ];
       experimental-features = [ "nix-command" "flakes" ];
