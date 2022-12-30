5b605216dfae5a801844caa932549cb1d13e0ad7
michelav <michel.vasconcelos@gmail.com>
2022-12-30 14:50:49 -0300

    Enable gpu profiles and mpris to mpv

--- home/michel/desktop/common/media.nix
+++ home/michel/desktop/common/media.nix
@@ -1,4 +1,19 @@
 { pkgs, lib, ... }: {
   home.packages = with pkgs; [ pavucontrol spotify-nss-latest playerctl ];
-  programs.mpv.enable = true;
+  programs.mpv = with pkgs; {
+    enable = true;
+    config = {
+      profile = "gpu-hq";
+      ytdl-format = "bestvideo+bestaudio";
+      vo = "gpu";
+    };
+    profiles = {
+      fast = { vo = "vdpau"; };
+      "protocol.dvd" = {
+        profile-desc = "profile for dvd:// streams";
+        alang = "en";
+      };
+    };
+    scripts = [ mpvScripts.mpris ];
+  };
 }
