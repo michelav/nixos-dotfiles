39a8a3161e0a49e130a8265b0c263c4bbf91a665
michelav <michel.vasconcelos@gmail.com>
2023-02-02 15:46:09 -0300

    Bump Flake Lock

--- flake.nix
+++ flake.nix
@@ -8,6 +8,7 @@
       # https://github.com/nix-community/neovim-nightly-overlay/issues/164
       inputs.nixpkgs.url =
         "github:nixos/nixpkgs?rev=fad51abd42ca17a60fc1d4cb9382e2d79ae31836";
+
       # inputs.nixpkgs.follows = "nixpkgs";
     };
     nixpkgs.url = "nixpkgs/nixos-unstable";
