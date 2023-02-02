6574bc265c44bf9a2782ad8e02a052f639a53816
michelav <michel.vasconcelos@gmail.com>
2023-02-02 17:21:14 -0300

    Add virtualisation capabilities

--- system/nixos/virtualisation.nix
+++ system/nixos/virtualisation.nix
@@ -1,10 +1,15 @@
 { pkgs, config, ... }: {
-  virtualisation.docker = {
-    enable = true;
-    enableNvidia = true;
-    enableOnBoot = false;
+  virtualisation = {
+    libvirtd.enable = true;
+    docker = {
+      enable = true;
+      enableNvidia = true;
+      enableOnBoot = false;
+    };
   };
 
+  environment.systemPackages = [ pkgs.virt-manager pkgs.libguestfs ];
+
   hardware.opengl.driSupport32Bit = true;
-  users.users.michel.extraGroups = [ "docker" ];
+  users.users.michel.extraGroups = [ "docker" "libvirtd" ];
 }
