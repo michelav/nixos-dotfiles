391a69185f48f1fc0ed5dba45e5093c8d3deb7ad
michelav <michel.vasconcelos@gmail.com>
2023-07-01 11:11:11 -0300

    Fix Maestral Config

--- home/michel/desktop/wayland/_common/maestral.nix
+++ home/michel/desktop/wayland/_common/maestral.nix
@@ -1,31 +1,5 @@
-{ pkgs, ... }:
-let maestral_cfg = ".config/maestral/maestral.ini";
-in {
+{ pkgs, ... }: {
   home.packages = [ pkgs.maestral pkgs.maestral-gui ];
-
-  /* home.file."${maestral_cfg}".text = ''
-       [auth]
-       account_id = dbid:AACJ2nkgBKE8yTy9I-l8n-phTgAbCUK-XeQ
-       keyring = keyring.backends.SecretService.Keyring
-
-       [app]
-       notification_level = 15
-       log_level = 20
-       update_notification_interval = 604800
-
-       [sync]
-       path = /persist/home/michel/Dropbox
-       excluded_items = ['/clouddetours', '/descritores', '/unifor', '/sara-15anos', '/uece tcc', '/epson iprint', '/avaliação tcc', '/recibos pacientes', '/trabs_quel', '/epson connect', '/sequentia']
-       reindex_interval = 1209600
-       max_cpu_percent = 20.0
-       keep_history = 604800
-       upload = True
-       download = True
-
-       [main]
-       version = 18.0
-     '';
-  */
 
   systemd.user.services.maestral-daemon = {
     Unit = { Description = "Maestral Dropbox Client"; };
