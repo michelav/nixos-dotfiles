f2e40b897f5ac6f61b622a85024c91c83fceebde
michelav <michel.vasconcelos@gmail.com>
2023-07-20 16:03:07 -0300

    Config intel gpu as default to hyprland

--- home/michel/desktop/wayland/hyprland/default.nix
+++ home/michel/desktop/wayland/hyprland/default.nix
@@ -1,10 +1,24 @@
-{ inputs, config, pkgs, ... }: {
+{ inputs, config, pkgs, ... }:
+let
+  hyprland-vega = pkgs.writeTextFile {
+    name = "hyprland-vega";
+    destination = "/bin/hyprland-vega";
+    executable = true;
+    text = let
+      intel = "0000:00:02.0";
+      # nvidia = "0000:00:01.0";
+    in ''
+      export WLR_DRM_DEVICES=$(udevadm info -q property --value -n /dev/dri/by-path/pci-${intel}-card | grep /dev/dri/card)
+      Hyprland
+    '';
+  };
+in {
   imports = [ inputs.hyprland.homeManagerModules.default ./wofi.nix ./eww ];
-  home.packages = [ pkgs.swaybg pkgs.hyprpicker ];
+  home.packages = [ pkgs.swaybg pkgs.hyprpicker hyprland-vega ];
   programs = {
     fish.loginShellInit = ''
       if test (tty) = "/dev/tty1"
-        exec Hyprland &> /dev/null
+        exec hyprland-vega &> /dev/null
       end
     '';
     zsh.loginExtra = ''
