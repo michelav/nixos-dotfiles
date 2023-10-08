54d37f172a4165308be3f51694371e41e848b004
michelav <michel.vasconcelos@gmail.com>
2023-10-08 18:09:11 -0300

    Flake Lock Update

--- home/michel/cli/neovim/ui.nix
+++ home/michel/cli/neovim/ui.nix
@@ -21,10 +21,10 @@
       config = ''
         vim.opt.list = true
         vim.opt.listchars:append("eol:↴")
-        require("indent_blankline").setup {
-          show_end_of_line = true,
-          show_current_context = true,
-          show_current_context_start = true
+        require("ibl").setup {
+          indent = {
+            char = "│",
+          }
         }
       '';
     }
