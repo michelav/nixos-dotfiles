e504fc4d1218a462fd5a96cd525419bbb40feb45
michelav <michel.vasconcelos@gmail.com>
2024-01-17 12:38:14 -0300

    Add default system configs for virt-manager

--- hosts/common/global/virtualisation.nix
+++ hosts/common/global/virtualisation.nix
@@ -3,8 +3,31 @@
     ./docker.nix
     # ./podman.nix
   ];
-  virtualisation.libvirtd.enable = true;
 
-  environment.systemPackages = [ pkgs.virt-manager pkgs.libguestfs ];
+  # Install necessary packages
+  environment.systemPackages = with pkgs; [
+    libguestfs
+    virt-manager
+    virt-viewer
+    spice
+    spice-gtk
+    spice-protocol
+    win-virtio
+    win-spice
+  ];
+
+  # Manage the virtualisation services
+  virtualisation = {
+    libvirtd = {
+      enable = true;
+      qemu = {
+        swtpm.enable = true;
+        ovmf.enable = true;
+        ovmf.packages = [ pkgs.OVMFFull.fd ];
+      };
+    };
+    spiceUSBRedirection.enable = true;
+  };
+  services.spice-vdagentd.enable = true;
 
 }
