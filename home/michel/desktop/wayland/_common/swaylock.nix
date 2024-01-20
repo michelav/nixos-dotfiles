80822ed88a71cd9c596941e5e095a44dcfbe2e72
michelav <michel.vasconcelos@gmail.com>
2024-01-20 17:21:21 -0300

    Customize swaylock look

--- home/michel/desktop/wayland/_common/swaylock.nix
+++ home/michel/desktop/wayland/_common/swaylock.nix
@@ -3,45 +3,48 @@
 let
   inherit (config.colorscheme) colors;
   inherit (config.userPrefs) fonts wallpaper;
-  swaylock = "${pkgs.swaylock-effects-main}/bin/swaylock";
+  swaylock = "${pkgs.swaylock-effects}/bin/swaylock";
 in {
   programs.swaylock = {
     enable = true;
     package = pkgs.swaylock-effects;
     settings = {
-      effect-blur = "10x2";
+      effect-blur = "5x2";
       fade-in = 0.5;
       grace = 2;
+      ignore-empty-password = true;
 
-      font = fonts.monospace.name;
-      font-size = 20;
+      font = fonts.regular.name;
+      # font-size = 20;
 
       line-uses-inside = true;
       clock = true;
       disable-caps-lock-text = true;
       indicator-caps-lock = true;
-      indicator-radius = 80;
+      indicator-radius = 150;
+      indicator-thickness = 15;
       indicator-idle-visible = true;
-      #      indicator-y-position = 1000;
 
-      ring-color = "#${colors.base02}";
-      inside-wrong-color = "#${colors.base08}";
+      ring-color = "#${colors.base07}88";
+      # inside-wrong-color = "#${colors.base08}";
+      inside-wrong-color = "#${colors.base01}22";
       ring-wrong-color = "#${colors.base08}";
       key-hl-color = "#${colors.base0B}";
-      bs-hl-color = "#${colors.base08}";
+      bs-hl-color = "#${colors.base01}";
       ring-ver-color = "#${colors.base09}";
-      inside-ver-color = "#${colors.base09}";
-      inside-color = "#${colors.base01}";
+      # inside-ver-color = "#${colors.base09}";
+      inside-ver-color = "#${colors.base01}22";
+      inside-color = "#${colors.base01}22";
       text-color = "#${colors.base07}";
-      text-clear-color = "#${colors.base01}";
-      text-ver-color = "#${colors.base01}";
-      text-wrong-color = "#${colors.base01}";
-      text-caps-lock-color = "#${colors.base07}";
-      inside-clear-color = "#${colors.base0C}";
+      text-clear-color = "#${colors.base0C}";
+      text-ver-color = "#${colors.base0A}";
+      text-wrong-color = "#${colors.base08}";
+      text-caps-lock-color = "#${colors.base09}";
+      inside-clear-color = "#${colors.base01}22";
       ring-clear-color = "#${colors.base0C}";
-      inside-caps-lock-color = "#${colors.base09}";
-      ring-caps-lock-color = "#${colors.base02}";
-      separator-color = "#${colors.base02}";
+      inside-caps-lock-color = "#${colors.base01}22";
+      ring-caps-lock-color = "#${colors.base09}";
+      separator-color = "#${colors.base02}00";
     };
   };
   systemd.user.services.lockOnAutoLogin = {
