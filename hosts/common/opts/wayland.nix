91206cbc7d8527392ac51dcd419bb582e2fc3030
michelav <michel.vasconcelos@gmail.com>
2024-01-27 12:41:57 -0300

    Add keyring to portal

--- hosts/common/opts/wayland.nix
+++ hosts/common/opts/wayland.nix
@@ -1,10 +1,13 @@
-{ pkgs, ... }: {
+{ pkgs, ... }:
+let keyring = pkgs.gnome.gnome-keyring;
+in {
   imports = [ ./greetd.nix ];
   services = {
     # gnome.gnome-keyring.enable = true;
-    # dbus.packages = [ pkgs.gcr ];
+    dbus.packages = [ pkgs.gcr keyring ];
     blueman.enable = true;
   };
+  xdg.portal.extraPortals = [ keyring ];
   security.pam.services.swaylock = { enableGnomeKeyring = true; };
   programs = {
     seahorse.enable = true;
