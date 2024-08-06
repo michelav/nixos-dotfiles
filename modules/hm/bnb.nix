2e52174d699d15f0de4ff7a9b77a7743d97ca33e
michelav <michel.vasconcelos@gmail.com>
2024-08-06 11:23:05 -0300

    feat: Add SAML capabilities to VPN Clients

--- modules/hm/bnb.nix
+++ modules/hm/bnb.nix
@@ -1,25 +1,47 @@
-{ inputs, pkgs, config, lib, ... }:
+{
+  inputs,
+  pkgs,
+  config,
+  lib,
+  ...
+}:
 let
   cfg = config.bnb.vpn;
-  inherit (lib) types mkOption mkEnableOption mkIf mapAttrsToList concatStrings;
+  inherit (lib)
+    types
+    mkOption
+    mkEnableOption
+    mkIf
+    mapAttrsToList
+    concatStrings
+    ;
   inherit (config) xdg;
   inherit (inputs.nixpkgs-stable.legacyPackages."x86_64-linux") remmina;
+  gpclient = inputs.gp-openconnect.packages."x86_64-linux".default;
   bnb-vpn = pkgs.writeTextFile {
     name = "bnb-vpn";
     destination = "/bin/bnb-vpn";
     executable = true;
-    text = let
-      oc = "${pkgs.openconnect}/bin/openconnect";
-      hip = "${pkgs.openconnect}/libexec/openconnect/hipreport.sh";
-    in ''
-      sudo ${oc} --config=${xdg.configHome}/bnb/vpn-config --protocol=gp --csd-wrapper=${hip} "$@"
-    '';
+    text =
+      let
+        oc = "${pkgs.openconnect}/bin/openconnect";
+        hip = "${pkgs.openconnect}/libexec/openconnect/hipreport.sh";
+      in
+      ''
+        sudo -E ${oc} --config=${xdg.configHome}/bnb/vpn-config --protocol=gp --csd-wrapper=${hip} "$@"
+      '';
   };
-in {
+in
+{
   options.bnb.vpn = {
     enable = mkEnableOption "Wether enable vpn for BNB";
     settings = mkOption {
-      type = with types; attrsOf (oneOf [ bool str ]);
+      type =
+        with types;
+        attrsOf (oneOf [
+          bool
+          str
+        ]);
       default = { };
       description = ''
         Default arguments to connect to {command}`BNB VPN`. An empty set
@@ -32,14 +54,19 @@ in {
     };
   };
   config = mkIf cfg.enable {
-    home.packages = [ pkgs.openconnect remmina bnb-vpn ];
+    home.packages = [
+      pkgs.openconnect
+      pkgs.gp-saml-gui
+      remmina
+      bnb-vpn
+      gpclient
+    ];
     xdg.configFile."bnb/vpn-config" = mkIf (cfg.settings != { }) {
-      text = concatStrings (mapAttrsToList (n: v:
-        if v == false then
-          ""
-        else
-          (if v == true then n else n + "=" + builtins.toString v) + "\n")
-        cfg.settings);
+      text = concatStrings (
+        mapAttrsToList (
+          n: v: if v == false then "" else (if v == true then n else n + "=" + builtins.toString v) + "\n"
+        ) cfg.settings
+      );
     };
   };
 }
