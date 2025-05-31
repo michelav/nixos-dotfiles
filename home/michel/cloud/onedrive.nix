8a62e5098bcc251f6dd39e447dbc4020e0910ffb
michelav <michel.vasconcelos@gmail.com>
2025-05-31 10:06:49 -0300

    chore: Fix onedrive service dependencies to uwsm

--- home/michel/cloud/onedrive.nix
+++ home/michel/cloud/onedrive.nix
@@ -1,25 +1,30 @@
-{ pkgs, ... }: {
+{ pkgs, ... }:
+{
 
   systemd.user.services."onedrive" = {
     Unit = {
       Description = "Rclone OneDrive Client";
-      After = [ "lockOnAutoLogin.service" ];
+      After = [ "graphical-session.target" ];
     };
-    Install = { WantedBy = [ "hyprland-session.target" ]; };
-    Service = let
-      dir = "/persist/home/michel/storages/onedrive";
-      rclone = "${pkgs.rclone}/bin/rclone";
-      fusermount = "${pkgs.fuse}/bin/fusermount";
-    in {
-      Type = "notify";
-      NotifyAccess = "exec";
-      Environment = [ "PATH=/run/wrappers/bin/:$PATH" ];
-      ExecStart = ''
-        ${rclone} mount onedrive: ${dir} --vfs-cache-mode full --dir-cache-time 48h \
-        --vfs-cache-max-age 48h --vfs-read-chunk-size 10M --vfs-read-chunk-size-limit 512M \
-        --buffer-size 512M --allow-other
-      '';
-      ExecStop = "${fusermount} -u ${dir}";
+    Install = {
+      WantedBy = [ "graphical-session.target" ];
     };
+    Service =
+      let
+        dir = "/persist/home/michel/storages/onedrive";
+        rclone = "${pkgs.rclone}/bin/rclone";
+        fusermount = "${pkgs.fuse}/bin/fusermount";
+      in
+      {
+        Type = "notify";
+        NotifyAccess = "exec";
+        Environment = [ "PATH=/run/wrappers/bin/:$PATH" ];
+        ExecStart = ''
+          ${rclone} mount onedrive: ${dir} --vfs-cache-mode full --dir-cache-time 48h \
+          --vfs-cache-max-age 48h --vfs-read-chunk-size 10M --vfs-read-chunk-size-limit 512M \
+          --buffer-size 512M --allow-other
+        '';
+        ExecStop = "${fusermount} -u ${dir}";
+      };
   };
 }
