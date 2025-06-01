f24b3510c47b764702c64bf120fd51541ebfbbfe
michelav <michel.vasconcelos@gmail.com>
2025-05-31 21:00:44 -0300

    updt: Update flake lock
    Got a problem when building nvidia drivers. Pinned nixpkgs
    while the issue doesnt get fixed.

--- flake.nix
+++ flake.nix
@@ -6,7 +6,12 @@
       url = "github:nix-community/neovim-nightly-overlay";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-    nixpkgs.url = "nixpkgs/nixos-unstable";
+
+    # nixpkgs.url = "nixpkgs/nixos-unstable";
+    # FIXME: Problem with Nvidia driver. Keeping this version of nixpkgs
+    # until the following patch lands unstable
+    # https://github.com/NixOS/nixpkgs/pull/412157
+    nixpkgs.url = "github:NixOS/nixpkgs/0271935b80f3b94588e40dee1b7abc3c6cb6e18d";
     nixpkgs-stable.url = "nixpkgs/23.11";
     flake-utils.url = "github:numtide/flake-utils";
     hardware.url = "github:nixos/nixos-hardware";
@@ -55,7 +60,7 @@
 
     ghostty = {
       url = "github:ghostty-org/ghostty";
-      inputs.nixpkgs-unstable.follows = "nixpkgs";
+      inputs.nixpkgs.follows = "nixpkgs";
     };
   };
 
