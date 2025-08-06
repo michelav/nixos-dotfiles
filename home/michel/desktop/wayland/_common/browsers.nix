b7d4f83237d32bf2d22d9746841d285aae3a15f8
michelav <michel.vasconcelos@gmail.com>
2025-08-06 09:33:47 -0300

    fix(firefox): Remove firefox warnings

--- home/michel/desktop/wayland/_common/browsers.nix
+++ home/michel/desktop/wayland/_common/browsers.nix
@@ -1,68 +1,76 @@
-{ inputs, pkgs, config, ... }: {
+{
+  inputs,
+  pkgs,
+  config,
+  ...
+}:
+{
   imports = [ ./qutebrowser.nix ];
   programs = {
     browserpass.enable = true;
     brave.enable = true;
-    firefox = let addons = inputs.firefox-addons.packages.${pkgs.system};
-    in {
-      enable = true;
-      profiles.michel = {
-        extensions = with addons; [
-          keepassxc-browser
-          ublock-origin
-          browserpass
-          vimium
-          # notion-web-clipper
-        ];
-        settings = {
-          "browser.disableResetPrompt" = true;
-          "browser.download.useDownloadDir" = false;
-          "dom.security.https_only_mode" = true;
-          "identity.fxaccounts.enabled" = false;
-          "privacy.trackingprotection.enabled" = true;
-          "signon.rememberSignons" = false;
-          "browser.newtabpage.activity-stream.showSponsoredTopSites" = false;
-          "browser.shell.checkDefaultBrowser" = false;
-          "browser.shell.defaultBrowserCheckCount" = 1;
-          "browser.download.panel.shown" = true;
-          "layout.css.prefers-color-scheme.content-override" = 1; # Browser Mode
-        };
-        search = {
-          default = "Google";
-          force = true;
-          engines = {
-            "Nix Packages" = {
-              urls = [{
-                template = "https://search.nixos.org/packages";
-                params = [
+    firefox =
+      let
+        addons = inputs.firefox-addons.packages.${pkgs.system};
+      in
+      {
+        enable = true;
+        profiles.michel = {
+          extensions.packages = with addons; [
+            ublock-origin
+            browserpass
+          ];
+          settings = {
+            "browser.disableResetPrompt" = true;
+            "browser.download.useDownloadDir" = false;
+            "dom.security.https_only_mode" = true;
+            "identity.fxaccounts.enabled" = false;
+            "privacy.trackingprotection.enabled" = true;
+            "signon.rememberSignons" = false;
+            "browser.newtabpage.activity-stream.showSponsoredTopSites" = false;
+            "browser.shell.checkDefaultBrowser" = false;
+            "browser.shell.defaultBrowserCheckCount" = 1;
+            "browser.download.panel.shown" = true;
+          };
+          search = {
+            default = "google";
+            force = true;
+            engines = {
+              "Nix Packages" = {
+                urls = [
                   {
-                    name = "type";
-                    value = "packages";
-                  }
-                  {
-                    name = "query";
-                    value = "{searchTerms}";
+                    template = "https://search.nixos.org/packages";
+                    params = [
+                      {
+                        name = "type";
+                        value = "packages";
+                      }
+                      {
+                        name = "query";
+                        value = "{searchTerms}";
+                      }
+                    ];
                   }
                 ];
-              }];
 
-              icon =
-                "${pkgs.nixos-icons}/share/icons/hicolor/scalable/apps/nix-snowflake.svg";
-              definedAliases = [ "@np" ];
-            };
+                icon = "${pkgs.nixos-icons}/share/icons/hicolor/scalable/apps/nix-snowflake.svg";
+                definedAliases = [ "@np" ];
+              };
 
-            "NixOS Wiki" = {
-              urls = [{
-                template = "https://wiki.nixos.org/index.php?search={searchTerms}";
-              }];
-              iconUpdateURL = "https://wiki.nixos.org/favicon.png";
-              updateInterval = 24 * 60 * 60 * 1000; # every day
-              definedAliases = [ "@nw" ];
+              "NixOS Wiki" = {
+                urls = [
+                  {
+                    template = "https://wiki.nixos.org/index.php?search={searchTerms}";
+                  }
+                ];
+                icon = "https://wiki.nixos.org/favicon.png";
+                updateInterval = 24 * 60 * 60 * 1000; # every day
+                definedAliases = [ "@nw" ];
+              };
             };
           };
         };
       };
-    };
     chromium = {
       enable = true;
       package = pkgs.google-chrome;
@@ -72,12 +80,22 @@
   };
 
   xdg.mimeApps.defaultApplications = {
-    "text/html" = [ "firefox.desktop" "org.qutebrowser.qutebrowser.desktop" ];
-    "text/xml" = [ "firefox.desktop" "org.qutebrowser.qutebrowser.desktop" ];
-    "x-scheme-handler/http" =
-      [ "firefox.desktop" "org.qutebrowser.qutebrowser.desktop" ];
-    "x-scheme-handler/https" =
-      [ "firefox.desktop" "org.qutebrowser.qutebrowser.desktop" ];
+    "text/html" = [
+      "firefox.desktop"
+      "org.qutebrowser.qutebrowser.desktop"
+    ];
+    "text/xml" = [
+      "firefox.desktop"
+      "org.qutebrowser.qutebrowser.desktop"
+    ];
+    "x-scheme-handler/http" = [
+      "firefox.desktop"
+      "org.qutebrowser.qutebrowser.desktop"
+    ];
+    "x-scheme-handler/https" = [
+      "firefox.desktop"
+      "org.qutebrowser.qutebrowser.desktop"
+    ];
     "x-scheme-handler/chrome" = [ "firefox.desktop" ];
     "x-scheme-handler/qute" = [ "org.qutebrowser.qutebrowser.desktop" ];
   };
