1874f4c6626d183082a882604314ae23b57efb20
michelav <michel.vasconcelos@gmail.com>
2025-08-06 09:32:56 -0300

    core(gpg): Remove gpg warnings

--- home/michel/cli/gpg.nix
+++ home/michel/cli/gpg.nix
@@ -1,20 +1,29 @@
 { config, pkgs, ... }:
 let
-  pinentry = if config.gtk.enable then {
-    packages = [ pkgs.pinentry-gnome3 pkgs.gcr ];
-    pkg = pkgs.pinentry-gnome3;
-  } else {
-    packages = [ pkgs.pinentry-curses ];
-    pkg = pkgs.pinentry-curses;
-  };
-in {
+  pinentry =
+    if config.gtk.enable then
+      {
+        packages = [
+          pkgs.pinentry-gnome3
+          pkgs.gcr
+        ];
+        pkg = pkgs.pinentry-gnome3;
+      }
+    else
+      {
+        packages = [ pkgs.pinentry-curses ];
+        pkg = pkgs.pinentry-curses;
+      };
+in
+{
   services.gpg-agent = {
     enable = true;
     enableFishIntegration = true;
-    # enableSshSupport = true;
-    pinentryPackage = pinentry.pkg;
+    pinentry.package = pinentry.pkg;
   };
 
-  programs.gpg = { enable = true; };
+  programs.gpg = {
+    enable = true;
+  };
 
 }
