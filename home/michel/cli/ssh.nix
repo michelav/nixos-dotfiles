937615d056f1f3d1c8c751e429b1f5b6d962045c
michelav <michel.vasconcelos@gmail.com>
2026-06-01 18:53:22 -0300

    chore(ssh): fix ssh warnings

--- home/michel/cli/ssh.nix
+++ home/michel/cli/ssh.nix
@@ -9,12 +9,21 @@
 
   programs.ssh = {
     enable = true;
-    extraConfig = ''
-      AddKeysToAgent yes
-    '';
-    matchBlocks = {
+    enableDefaultConfig = false;
+    settings = {
       "*" = {
         compression = true;
+        ForwardAgent = false;
+        AddKeysToAgent = "yes";
+
+        ServerAliveInterval = 0;
+        ServerAliveCountMax = 3;
+        HashKnownHosts = false;
+        UserKnownHostsFile = "~/.ssh/known_hosts";
+
+        ControlMaster = "no";
+        ControlPath = "~/.ssh/master-%r@%n:%p";
+        ControlPersist = "no";
       };
       "github.com" = {
         identityFile = with config.home; "${homeDirectory}/.ssh/michelav_github";
